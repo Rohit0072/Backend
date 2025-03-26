b5024b97ab49d1e1afa27d5a27cb86dc89f7e992
Rohit Singh <rs3274357@gmail.com>
2025-03-26 14:09:37 +0530

    change the port

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.use((err, req, res, next) => {
 });
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
